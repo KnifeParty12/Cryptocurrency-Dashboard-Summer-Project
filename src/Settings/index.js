23ce5513837af30c42428568524fb3d821ec0fe4
KnifeParty12 <flashfunk01@gmail.com>
2019-07-23 03:33:33 +0530

    Added searchbar
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -3,13 +3,14 @@ import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
 import CoinGrid from './CoinGrid'
-
+import Search from './Search';
 
 export default function () {
     return <Page name="settings">
     <WelcomeMessage/>
     <CoinGrid topSection/>
     <ConfirmButton/>
+    <Search/>
     <CoinGrid/>
     </Page>
     }
