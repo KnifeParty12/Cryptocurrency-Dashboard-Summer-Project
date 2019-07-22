2ff1601e2cbfbb3e0224cc215bac61b4f8f8c127
KnifeParty12 <flashfunk01@gmail.com>
2019-07-23 01:21:34 +0530

    add loading content
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App/index.js
+++ src/App/index.js
@@ -4,14 +4,16 @@ import AppLayout from './AppLayout';
 import AppBar from './AppBar';
 import {AppProvider} from './AppProvider';
 import Settings from '../Settings';
-
+import Content from '../Shared/Content';
 
 function App() {
   return (
       <AppLayout>
           <AppProvider>
           <AppBar/>
+          <Content>
       <Settings/>
+          </Content>
           </AppProvider>
       </AppLayout>
       );
