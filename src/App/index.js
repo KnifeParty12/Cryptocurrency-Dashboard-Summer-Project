5551d735cdcd06b881689e4b4ac0dca7b54a4ba4
KnifeParty12 <flashfunk01@gmail.com>
2019-07-22 23:23:47 +0530

    Added Appbar to grid
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App/index.js
+++ src/App/index.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 function App() {
   return (
       <AppLayout>
+          <AppBar/>
       <WelcomeMessage/>
       </AppLayout>
       );
