c9091b55be0c590995403d2e304a4acd19d11445
KnifeParty12 <flashfunk01@gmail.com>
2019-07-22 23:18:04 +0530

    Add app layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import AppLayout from './AppLayout';
 
 function App() {
   return (
-      <div> Hello World</div>
-  );
+      <AppLayout>
+      <WelcomeMessage/>
+      </AppLayout>
+      );
 }
 
 export default App;
