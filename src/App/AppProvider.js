8bb838030e16ab9360b3632cca43507b7739921c
KnifeParty12 <flashfunk01@gmail.com>
2019-07-23 01:11:18 +0530

    Added Cryptocompare
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const cc = require('cryptocompare');
+
 export const AppContext = React.createContext();
 
 export class AppProvider extends React.Component{
@@ -12,6 +14,15 @@ export class AppProvider extends React.Component{
             confirmFavorites: this.confirmFavorites
         }
     }
+
+    componentDidMount = () => {
+    this.fetchCoins();
+    };
+
+    fetchCoins = async () => {
+        let coinList = (await cc.coinList()).Data;
+        this.setState(coinList);
+    };
 
     confirmFavorites = () => {
         this.setState({
