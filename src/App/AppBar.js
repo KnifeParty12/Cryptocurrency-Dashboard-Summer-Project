9ecc1b238def0c900199e41b35fdf10ee4653cc5
KnifeParty12 <flashfunk01@gmail.com>
2019-07-22 23:40:51 +0530

    Styled AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+
+
+const Logo = styled.div`
+font-size: 1.5em;
+`
 
 const Bar = styled.div`
 display: grid;
 grid-template-columns: 180px auto 100px 100px;
 `
 
+const ControlButtonElem = styled.div`
+cursor:pointer;
+${ props => props.active && css`
+margin-bottom: 40px;
+text-shadow: 0px 0px 60px #03ff03;
+`}
+`
+
+function toProperCase(lower) {
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControlButton({name, active}) {
+    return (<ControlButtonElem active={active}>
+        {toProperCase(name)}
+    </ControlButtonElem>)
+}
+
 export default function () {
-    return <Bar>
+    return  (<Bar>
         <div> CryptoDash </div>
         <div/>
-        <div> Dashboard </div>
-        <div> Settings</div>
-    </Bar>
+        <ControlButton active name = "dashboard"/>
+        <ControlButton name="settings"/>
+    </Bar>)
 }
