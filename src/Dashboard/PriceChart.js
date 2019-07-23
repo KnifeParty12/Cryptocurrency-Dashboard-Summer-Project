c6b29fa990e28a4666e1b0e0613a99bbed06e86a
KnifeParty12 <flashfunk01@gmail.com>
2019-07-23 17:35:11 +0530

    Introduced and edited highcharts theme
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -3,6 +3,9 @@ import React from 'react';
 import {Tile} from "../Shared/Tile";
 import {AppContext} from "../App/AppProvider";
 import ReactHighcharts from 'react-highcharts';
+import HighchartsTheme from './HighchartsTheme';
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
+
 
 export default function () {
     return(
